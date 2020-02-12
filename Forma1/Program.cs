b4c3eef7f00d038f29fc3e6693398c025f51a6cd
István Bálint <istvann.balint@gmail.com>
2020-02-12 14:21:31 +0100

    Modell versenyzők

--- Forma1/Program.cs
+++ Forma1/Program.cs
@@ -16,7 +16,7 @@ namespace Forma1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1Form());
+            Application.Run(new Form1Form()); 
         }
     }
 }
