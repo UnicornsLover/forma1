cc03188a0d96f03204f373b6918e2d99986c86f8
István Bálint <istvann.balint@gmail.com>
2020-02-12 14:25:38 +0100

    repository Teanm

---
+++ Forma1/Repository/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forma1.Repository
+{
+    /// <summary>
+    /// A Forma1-ben szereplő csapatok
+    /// </summary>
+    class Team
+    {
+
+    }
+}
